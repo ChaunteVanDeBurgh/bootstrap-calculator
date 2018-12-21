59f7fac8f55004b7d41af892a4ba14f6d6587d78
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2018-12-20 22:44:36 -0600

    create addition function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+let no_value = true;
+let first_value;
+let second_value;
+let solution;
+
+const display = (x) => {
+	document.getElementById('display').value = x;
+}
+
 const writedisplay = (x) => {
 	let current_display = document.getElementById("display").value;
 	let input = (x);
@@ -13,4 +22,25 @@ const writedisplay = (x) => {
 const cleardisplay = () => {
 	const clear_value = '0'
 	document.getElementById("display").value = clear_value;
+};
+
+const ce = () => {
+	cleardisplay();
+	no_value = true;
+}
+
+const addition = () => {
+	if (no_value == true) {
+		first_value = document.getElementById('display').value;
+		no_value = false;
+		cleardisplay();
+	} else {
+		second_value = document.getElementById('display').value;
+		solution = Number(first_value) + Number(second_value);
+		display(solution);
+	}
+};
+
+const equal = () => {
+	display(solution);
 };
