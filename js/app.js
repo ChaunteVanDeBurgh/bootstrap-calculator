57b2f1905b8528dc09525122a026ca55d42c57f1
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2018-12-20 22:04:01 -0600

    fix leading zero issue

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 const writedisplay = (x) => {
 	let current_display = document.getElementById("display").value;
 	let input = (x);
-	next_display = current_display + input;
-	document.getElementById("display").value = next_display;
+
+	if (current_display == '0') {
+		document.getElementById("display").value = x;
+	} else {
+		next_display = current_display + input;
+		document.getElementById("display").value = next_display;
+	}
 };
 
 const cleardisplay = () => {
